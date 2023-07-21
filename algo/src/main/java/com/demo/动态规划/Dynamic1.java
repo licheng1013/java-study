5f4e5c214ccb502be3455440f67ea2005e450261
licheng <2501093733@qq.com>
2023-07-21 16:24:50 +0800

    增加两个序列和!

--- algo/src/main/java/com/demo/动态规划/Dynamic1.java
+++ algo/src/main/java/com/demo/动态规划/Dynamic1.java
@@ -1,8 +1,45 @@
 package com.demo.动态规划;
 
-public class Dynamic1 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Dynamic1 {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+        System.out.println(sequenceSum(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
+
+    // 爬楼梯解法
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+
+    // 最大子序和 [-2,1,-3,4,-1,2,1,-5,4] 输出 6
+    public static int sequenceSum(int[] nums){
+        int max = 0;
+        if (nums.length == 0) {
+            return max;
+        }
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0){
+                dp[i] = nums[i];
+            }else {
+                dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
 
 }
