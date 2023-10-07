640e521a850c28d5c7b8cff885b837847d1c0889
licheng10113 <2501093733@qq.com>
2023-10-07 22:40:29 +0800

    重构功能!

--- sb-web/src/main/java/org/example/api/UserApi.java
+++ sb-web/src/main/java/org/example/api/UserApi.java
@@ -1,6 +1,7 @@
 package org.example.api;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.model.Result;
 import org.example.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,22 +11,19 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/user")
 public class UserApi {
+
     @GetMapping("/list")
     public Object list(User user){
-        HashMap<Object, Object> map = new HashMap<>();
-        map.put("code",0);
-        map.put("msg","你好");
-        map.put("data",user);
-        return map;
+        user.setName("LIST");
+        user.setAge(101);
+        return Result.ok(user);
     }
 
     @RequestMapping("/insert")
     public Object insert(User user, HttpServletRequest request){
         user.setToken(request.getHeader("Token"));
-        HashMap<Object, Object> map = new HashMap<>();
-        map.put("code",0);
-        map.put("msg","你好");
-        map.put("data",user);
-        return map;
+        user.setName("测试");
+        user.setAge(20);
+        return Result.ok(user);
     }
 }
---
+++ sb-web/src/main/java/org/example/model/Result.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+public class Result<T>{
+    public int code = 0;
+    public String msg = "成功";
+
+    public T data;
+
+    public static <T> Result<T> ok(T data){
+        Result<T> result = new Result<>();
+        result.data = data;
+        return result;
+    }
+
+    public static Result<Object> error(String msg){
+        Result<Object> result = new Result<>();
+        result.code = 1;
+        result.msg = msg;
+        return result;
+    }
+
+    public static Result<Object> okMsg(String msg){
+        Result<Object> result = new Result<>();
+        result.msg = msg;
+        return result;
+    }
+}
